75551c195f2bd162da085247677b3202f813d442
Amanec031 <amankumarmenu@gmail.com>
2024-06-22 23:55:05 +0530

    styling of carousel done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -11,10 +11,10 @@ export default function Carousel() {
 
                 <div className="carousel-inner " id='carousel'>
                     <div class=" carousel-caption  " style={{ zIndex: "9" }}>
-                        <form className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
+                        <div className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
                             <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Type in..." aria-label="Search" />
                             <button className="btn text-white bg-success" type="submit">Search</button>
-                        </form>
+                        </div>
                     </div>
                     <div className="carousel-item active" >
                         <img src={burger} className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
